7f83471b370ed0017232065cf6dd14d005875d9d
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-29 08:14:19 -0400

    modified controller to match angular format

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,26 +5,18 @@ class CategoriesController < ApplicationController
     end
 
     def create
-        category = Category.new(name:params[:category])
-        if category.save
-            redirect_to '/#'
-        else
-            puts "damn, something went wrong"
-            redirect_to '/#'
-        end
+        Category.create(name:params[:category])
     end
 
     def update
         Category.update(params[:id], name:params[:category])
-        redirect_to '/#'
     end
 
     def destroy
         Category.destroy(params[:id])
-        redirect_to '/#'
     end
 
     def show
-        @category = Category.find(params[:id])
+        render :json => Category.find(params[:id])
     end
 end
