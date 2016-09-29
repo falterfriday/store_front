5ddf4289f51876e543ebc1ea5c1f6ee8b97a10e1
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-29 08:15:17 -0400

    modified controller to match angular format

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,12 @@ class CommentsController < ApplicationController
         comment = Comment.new(content:params[:content], user:current_user, product:params[:product_id])
         if comment.save
             puts "comment: success"
-            redirect_to '/#'
         else
             puts "comment: something went wrong"
-            redirect_to '/#'
         end
     end
 
     def destroy
         Comment.destroy(params[:id])
-        redirect_to '/#'
     end
 end
