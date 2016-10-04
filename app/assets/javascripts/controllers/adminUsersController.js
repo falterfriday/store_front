4af7f7cb86d2516b55a2c8205f6401af8a269d80
Patrick Todd <patrick.a.todd@gmail.com>
2016-10-04 13:25:36 -0400

    adminUsersController and Factory

---
+++ app/assets/javascripts/controllers/adminUsersController.js
@@ -0,0 +1,3 @@
+app.controller('adminUsersController', ['adminUsersFactory', '$scope', '$cookies', '$routeParams', '$location', function(adminUsersFactory, $scope, $cookies, $routeParams, $location){
+
+}]);
---
+++ app/assets/javascripts/factories/adminUsersFactory.js
@@ -0,0 +1,8 @@
+app.factory('adminUsersFactory', ['$http', function($http){
+
+    function adminUsersFactory(){
+        console.log("LOLOLOLOLOLOLOLOLOLOL");
+    }
+
+    return new adminUsersFactory();
+}]);
